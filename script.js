ae456cffc91ab4524a1076891c6cd9d340eec70e
BENCY R <bencyramesh2006@gmail.com>
2026-09-08 22:52:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// Smooth scroll when clicking navbar links
+
+document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', function(event) {
+        event.preventDefault();
+
+        const targetId = this.getAttribute('href');
+        const targetSection = document.querySelector(targetId);
+
+        targetSection.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
